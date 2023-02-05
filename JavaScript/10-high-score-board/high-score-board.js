38e3064d9201775053d5d3e29a18a9609fd84d40
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-02-05 16:06:38 +0100

    feat: add high-score-board 2nd challenge

--- JavaScript/10-high-score-board/high-score-board.js
+++ JavaScript/10-high-score-board/high-score-board.js
@@ -11,3 +11,16 @@ export function createScoreBoard() {
     "The Best Ever": 1000000,
   };
 }
+
+/**
+ * Adds a player to a score board.
+ *
+ * @param {Record<string, number>} scoreBoard
+ * @param {string} player
+ * @param {number} score
+ * @returns {Record<string, number>} updated score board
+ */
+export function addPlayer(scoreBoard, player, score) {
+  scoreBoard[player] = score;
+  return scoreBoard;
+}
