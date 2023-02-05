8f1875867c86a71d9a39a356eb27f31d16ac7122
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-02-05 16:06:00 +0100

    feat: add high-score-board 1st challenge

---
+++ JavaScript/10-high-score-board/high-score-board.js
@@ -0,0 +1,13 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new score board with an initial entry.
+ *
+ * @returns {Record<string, number>} new score board
+ */
+export function createScoreBoard() {
+  return {
+    "The Best Ever": 1000000,
+  };
+}
