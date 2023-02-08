4c1c354ed61f412d3cbed50e1c7ecf53fc7d8df7
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-02-08 20:35:15 +0100

    feat: add amusement-park 1st challenge

---
+++ JavaScript/11-amusement-park/amusement-park.js
@@ -0,0 +1,18 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new visitor.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @param {string} ticketId
+ * @returns {Visitor} the visitor that was created
+ */
+export function createVisitor(name, age, ticketId) {
+  return {
+    name,
+    age,
+    ticketId,
+  };
+}
