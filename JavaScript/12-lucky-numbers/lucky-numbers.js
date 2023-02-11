90ca6ccf3db49109e0ed3db4e6be3d1cba1b8022
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-02-11 16:21:18 +0100

    feat: add lucky-numbers 1st challenge

---
+++ JavaScript/12-lucky-numbers/lucky-numbers.js
@@ -0,0 +1,15 @@
+// @ts-check
+
+/**
+ * Calculates the sum of the two input arrays.
+ *
+ * @param {number[]} array1
+ * @param {number[]} array2
+ * @returns {number} sum of the two arrays
+ */
+export function twoSum(array1, array2) {
+  const num1 = Number(array1.join(""));
+  const num2 = Number(array2.join(""));
+
+  return num1 + num2;
+}
