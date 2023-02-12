b26f17008bf7e17fac34752983264870f37f3491
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-02-12 16:14:07 +0100

    feat: add lasagna-master 1st challenge

---
+++ JavaScript/13-lasagna-master/lasagna-master.js
@@ -0,0 +1,22 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Implement the functions needed to solve the exercise here.
+ * Do not forget to export them so they are available for the
+ * tests. Here an example of the syntax as reminder:
+ *
+ * export function yourFunction(...) {
+ *   ...
+ * }
+ */
+// Task 1
+export function cookingStatus(timer) {
+  if (timer === 0) {
+    return "Lasagna is done.";
+  } else if (timer === undefined) {
+    return "You forgot to set the timer.";
+  } else {
+    return "Not done, please wait.";
+  }
+}
