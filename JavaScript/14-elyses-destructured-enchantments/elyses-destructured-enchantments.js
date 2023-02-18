c45e5a2ac240863b952a3dba921c71624a7b7375
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-02-18 15:07:59 +0100

    feat: add array destructuring 1st challenge

---
+++ JavaScript/14-elyses-destructured-enchantments/elyses-destructured-enchantments.js
@@ -0,0 +1,14 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Get the first card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the first card in the deck
+ */
+export function getFirstCard(deck) {
+  const [firstCard] = deck;
+  return firstCard;
+}
