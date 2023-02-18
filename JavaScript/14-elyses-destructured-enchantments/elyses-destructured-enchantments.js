863b94e9e6ea12de5d36cee4f91c416c889523cb
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-02-18 15:08:27 +0100

    feat: add array destructuring 2nd challenge

--- JavaScript/14-elyses-destructured-enchantments/elyses-destructured-enchantments.js
+++ JavaScript/14-elyses-destructured-enchantments/elyses-destructured-enchantments.js
@@ -12,3 +12,15 @@ export function getFirstCard(deck) {
   const [firstCard] = deck;
   return firstCard;
 }
+
+/**
+ * Get the second card in the given deck
+ *
+ * @param {Card[]} deck
+ *
+ * @returns {Card} the second card in the deck
+ */
+export function getSecondCard(deck) {
+  const [_, secondCard] = deck;
+  return secondCard;
+}
