06f4c5a6eb28e0483a15920b15df496c63860141
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-02-19 16:17:27 +0100

    feat: add closures 1st challenge

---
+++ JavaScript/15-coordinate-transformation/coordinate-transformation.js
@@ -0,0 +1,21 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Create a function that returns a function making use of a closure to
+ * perform a repeatable 2d translation of a coordinate pair.
+ *
+ * @param {number} dx the translate x component
+ * @param {number} dy the translate y component
+ *
+ * @returns {function} a function which takes an x, y parameter, returns the
+ *  translated coordinate pair in the form [x, y]
+ */
+export function translate2d(dx, dy) {
+  return (x, y) => {
+    return [x + dx, y + dy];
+  };
+}
