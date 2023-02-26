42043894828a4161e6ad91416a23c483d2574ed5
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-02-26 15:57:06 +0100

    feat: add indexOf challenge

---
+++ JavaScript/17-elyses-analytic-enchantments/elyses-analytic-enchantments.js
@@ -0,0 +1,17 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Get the position (index) of the card in the given stack
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} position of the card in the stack
+ */
+export function getCardPosition(stack, card) {
+  return stack.indexOf(card);
+}
