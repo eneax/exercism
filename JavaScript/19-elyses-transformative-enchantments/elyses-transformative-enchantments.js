185579d54455251d5be561c310bfa58bc1f471a7
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-03-05 16:50:53 +0100

    feat: add array transformation 2nd challenge

--- JavaScript/19-elyses-transformative-enchantments/elyses-transformative-enchantments.js
+++ JavaScript/19-elyses-transformative-enchantments/elyses-transformative-enchantments.js
@@ -10,3 +10,19 @@
 export function seeingDouble(deck) {
   return deck.map((card) => card * 2);
 }
+
+/**
+ *  Creates triplicates of every 3 found in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with triplicate 3s
+ */
+export function threeOfEachThree(deck) {
+  return deck.reduce((acc, card) => {
+    if (card === 3) {
+      return [...acc, 3, 3, 3];
+    }
+    return [...acc, card];
+  }, []);
+}
