e519ef27da99b01ee36996672c4f0148961d461e
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-03-05 16:50:30 +0100

    feat: add array transformation 1st challenge

---
+++ JavaScript/19-elyses-transformative-enchantments/elyses-transformative-enchantments.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/**
+ * Double every card in the deck.
+ *
+ * @param {number[]} deck
+ *
+ * @returns {number[]} deck with every card doubled
+ */
+export function seeingDouble(deck) {
+  return deck.map((card) => card * 2);
+}
