4c52c19d6e5b13161182afdca597a9e33a1241c2
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-03-12 11:09:05 +0100

    refactor: update tasks to use class and prototype

--- JavaScript/21-windowing-system/windowing-system.js
+++ JavaScript/21-windowing-system/windowing-system.js
@@ -9,57 +9,49 @@
 export function Size(width = 80, height = 60) {
   this.width = width;
   this.height = height;
-  this.resize = function (newWidth, newHeight) {
-    this.width = newWidth;
-    this.height = newHeight;
-  };
 }
+
+Size.prototype.resize = function (newWidth, newHeight) {
+  this.width = newWidth;
+  this.height = newHeight;
+};
 
 export function Position(x = 0, y = 0) {
   this.x = x;
   this.y = y;
-  this.move = function (newX, newY) {
-    this.x = newX;
-    this.y = newY;
-  };
 }
 
-export function ProgramWindow() {
-  this.screenSize = new Size(800, 600);
-  this.size = new Size();
-  this.position = new Position();
+Position.prototype.move = function (newX, newY) {
+  this.x = newX;
+  this.y = newY;
+};
 
-  this.resize = function (newSize) {
-    if (newSize.width < 1) {
-      newSize.width = 1;
-    }
-    if (newSize.height < 1) {
-      newSize.height = 1;
-    }
-    if (newSize.width > this.screenSize.width - this.position.x) {
-      newSize.width = this.screenSize.width - this.position.x;
-    }
-    if (newSize.height > this.screenSize.height - this.position.y) {
-      newSize.height = this.screenSize.height - this.position.y;
-    }
-    this.size = newSize;
-  };
+export class ProgramWindow {
+  constructor() {
+    this.screenSize = new Size(800, 600);
+    this.size = new Size();
+    this.position = new Position();
+  }
 
-  this.move = function (newPosition) {
-    if (newPosition.x < 0) {
-      newPosition.x = 0;
-    }
-    if (newPosition.y < 0) {
-      newPosition.y = 0;
-    }
-    if (newPosition.x > this.screenSize.width - this.size.width) {
-      newPosition.x = this.screenSize.width - this.size.width;
-    }
-    if (newPosition.y > this.screenSize.height - this.size.height) {
-      newPosition.y = this.screenSize.height - this.size.height;
-    }
-    this.position = newPosition;
-  };
+  resize(newSize) {
+    const maxWidth = this.screenSize.width - this.position.x;
+    const maxHeight = this.screenSize.height - this.position.y;
+
+    const newWidth = Math.max(1, Math.min(newSize.width, maxWidth));
+    const newHeight = Math.max(1, Math.min(newSize.height, maxHeight));
+
+    this.size.resize(newWidth, newHeight);
+  }
+
+  move(newPosition) {
+    const maxX = this.screenSize.width - this.size.width;
+    const maxY = this.screenSize.height - this.size.height;
+    const newX = Math.max(0, Math.min(newPosition.x, maxX));
+
+    const newY = Math.max(0, Math.min(newPosition.y, maxY));
+
+    this.position.move(newX, newY);
+  }
 }
 
 export function changeWindow(window) {
