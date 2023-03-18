5ef80c1108c54045edec9e4e8bc5aaa985e274f4
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-03-18 15:38:10 +0100

    feat: add set 1st challenge

---
+++ JavaScript/22-ozans-playlist/ozans-playlist.js
@@ -0,0 +1,17 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Removes duplicate tracks from a playlist.
+ *
+ * @param {string[]} playlist
+ * @returns {string[]} new playlist with unique entries
+ */
+
+export function removeDuplicates(playlist) {
+  const uniqueTracks = new Set(playlist);
+  return [...uniqueTracks];
+}
