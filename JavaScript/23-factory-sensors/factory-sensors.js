3ae0d420016e84780903c4cb8b41322d857ea110
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-03-19 11:57:13 +0100

    feat: add errors and inheritance 3nd challenge

--- JavaScript/23-factory-sensors/factory-sensors.js
+++ JavaScript/23-factory-sensors/factory-sensors.js
@@ -18,7 +18,9 @@ export class OverheatingError extends Error {
 
 export function checkHumidityLevel(humidityPercentage) {
   if (humidityPercentage > 70) {
-    throw new Error();
+    throw new Error(
+      `Exceeding the maximum allowed humidity level of 70%! Current level: ${humidityPercentage}%`
+    );
   }
 }
 
@@ -31,8 +33,38 @@ export function checkHumidityLevel(humidityPercentage) {
 
 export function reportOverheating(temperature) {
   if (temperature === null) {
-    throw new ArgumentError();
+    throw new ArgumentError("Sensor is not working!");
   } else if (temperature > 500) {
     throw new OverheatingError(temperature);
+  }
+}
+
+/**
+ *  Triggers the needed action depending on the result of the machine check.
+ *
+ * @param {{
+ * check: function,
+ * alertDeadSensor: function,
+ * alertOverheating: function,
+ * shutdown: function
+ * }} actions
+ * @throws {ArgumentError|OverheatingError|Error}
+ */
+
+export function monitorTheMachine(actions) {
+  try {
+    actions.check();
+  } catch (error) {
+    if (error instanceof ArgumentError) {
+      actions.alertDeadSensor();
+    } else if (error instanceof OverheatingError) {
+      if (error.temperature < 600) {
+        actions.alertOverheating();
+      } else {
+        actions.shutdown();
+      }
+    } else {
+      throw error;
+    }
   }
 }
