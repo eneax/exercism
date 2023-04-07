d121e402499a0ee6ce55d3988d744419a8a528fb
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-04-07 10:15:33 +0200

    feat: add regex 2nd challenge

--- JavaScript/25-regular-chatbot/regular-chatbot.js
+++ JavaScript/25-regular-chatbot/regular-chatbot.js
@@ -11,3 +11,15 @@ export function isValidCommand(command) {
   const regex = /^chatbot, /i;
   return regex.test(command);
 }
+
+/**
+ * Given a certain message, help the chatbot get rid of all the emoji's encryption through the message.
+ *
+ * @param {string} message
+ * @returns {string} The message without the emojis encryption
+ */
+
+export function removeEmoji(message) {
+  const regex = /emoji\d+/g;
+  return message.replace(regex, "");
+}
