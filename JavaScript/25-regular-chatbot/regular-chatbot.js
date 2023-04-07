f48c77938f72a88564d1b5825ffb969a6a378b98
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-04-07 10:14:19 +0200

    feat: add regex 1st challenge

---
+++ JavaScript/25-regular-chatbot/regular-chatbot.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+/**
+ * Given a certain command, help the chatbot recognize whether the command is valid or not.
+ *
+ * @param {string} command
+ * @returns {boolean} whether or not is the command valid
+ */
+
+export function isValidCommand(command) {
+  const regex = /^chatbot, /i;
+  return regex.test(command);
+}
