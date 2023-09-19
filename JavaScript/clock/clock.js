817c2c2041a51441ff204addae33d7b98a6a6104
Enea Xharja <19818937+eneax@users.noreply.github.com>
2023-09-19 18:36:47 +0200

    docs: add docs about Clock challenge

---
+++ JavaScript/clock/clock.js
@@ -0,0 +1,26 @@
+//
+// This is only a SKELETON file for the 'Clock' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class Clock {
+  constructor() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  toString() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  plus() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  minus() {
+    throw new Error("Remove this statement and implement this function");
+  }
+
+  equals() {
+    throw new Error("Remove this statement and implement this function");
+  }
+}
---
+++ JavaScript/clock/clock.md
@@ -0,0 +1,11 @@
+# Clock
+
+## Instructions
+
+Implement a clock that handles times without dates.
+
+You should be able to add and subtract minutes to it.
+
+Two clocks that represent the same time should be equal to each other.
+
+Using the built-in [Date class](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date) and its methods is not allowed.
