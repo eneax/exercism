039c326270208c3dd17ab5ee3fd96068c31b9e1b
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-02-18 08:26:20 +0100

    docs: add docs about Rotational Cipher challenge

---
+++ JavaScript/rotational-cipher/rotational-cipher.js
@@ -0,0 +1,8 @@
+//
+// This is only a SKELETON file for the 'Rotational Cipher' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const rotate = () => {
+  throw new Error("Remove this statement and implement this function");
+};
